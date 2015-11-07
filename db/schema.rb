1503cd55b747c9ac987220b926238f3f3c05b33a
German Escobar <german.escobarc@gmail.com>
2015-11-06 22:26:08 -0500

    Implementa la funcionalidad de listar y crear pines

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107025809) do
+ActiveRecord::Schema.define(version: 20151107031032) do
+
+  create_table "pins", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.string   "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
